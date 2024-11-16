bae171c2a00d5584115caedc52d2952609896ca1
diego-garro <diego.garromolina@yahoo.com>
2024-11-15 19:52:13 -0600

    feat: configure CLI and add --version option

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,34 @@
+from typing import Optional, Annotated
+
+import typer
+from rich import print
+
 from .__version__ import __version__
 
-print(f"v{__version__}")
+
+app = typer.Typer()
+
+
+def version_callback(version: bool) -> None:
+    if version:
+        print(f"metar-datasets, version {__version__}")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    version: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--version",
+            "-v",
+            callback=version_callback,
+            help="Show module version and exit.",
+        ),
+    ] = None,
+) -> None:
+    print("metar-datasets CLI")
+
+
+if __name__ == "__main__":
+    app()
